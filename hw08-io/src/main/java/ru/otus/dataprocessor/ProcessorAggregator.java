9f2ce31c94910a31c9560064164be9e8319d88db
AlekSavvat <Aleksandr.Savvateev2@rusal.com>
2023-01-19 22:25:20 +0300

    получение TreeMap из stream сразу

--- hw08-io/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ hw08-io/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -11,11 +11,10 @@ public class ProcessorAggregator implements Processor {
     public Map<String, Double> process(List<Measurement> data) {
         //группирует выходящий список по name, при этом суммирует поля value
         try {
-            var resultMap =  data.stream()
-                    .collect(Collectors.groupingBy(Measurement::getName
+            return data.stream()
+                    .collect(Collectors.groupingBy(Measurement::getName, TreeMap::new
                             , Collectors.summingDouble(Measurement::getValue)));
-
-            return new TreeMap<String, Double>(resultMap);
+            
         }catch (Exception ex){
             throw new FileProcessException(ex);
         }
